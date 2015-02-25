e424fa3d33ac127ecce731143b8eb245da3b3371
kevim <kevim@flwks002>
2015-02-25 15:41:27 -0300

    getMilliseconds Added!!!

--- src/org/lemanoman/converters/TimeConverter.java
+++ src/org/lemanoman/converters/TimeConverter.java
@@ -15,6 +15,38 @@ public class TimeConverter {
 			resposta = calendar.get(Calendar.MINUTE)+" minutes "+calendar.get(Calendar.SECOND)+" seconds "+calendar.get(Calendar.MILLISECOND)+" milliseconds";
 		}
 		return resposta;
+	}
+	
+	/**
+	 * 
+	 * <b>Usage:</b> "time period" <br />
+	 * <b>Exemple:</b>  10 mins,20 days,10 hours
+	 **/
+	
+	public static long getMilliseconds(String value){
+		Integer tempo = Integer.parseInt(value.split(" ")[0]); 
+		String periodo = value.split(" ")[1].toLowerCase();
+		Calendar c = Calendar.getInstance();
+		c.setTimeInMillis(0);
+		
+		if(tempo!=null && periodo!=null && periodo!=""){
+			if(periodo.contains("day")){
+				c.set(Calendar.DAY_OF_YEAR, tempo);
+			}else if(periodo.contains("week")){
+				c.set(Calendar.WEEK_OF_YEAR, tempo);
+			}else if(periodo.contains("hour") || periodo.contains("hrs")){
+				c.set(Calendar.HOUR, tempo);
+			}else if(periodo.contains("minute") || periodo.contains("mins")){
+				c.set(Calendar.MINUTE, tempo);
+			}else if(periodo.contains("second") || periodo.contains("secs")){
+				c.set(Calendar.SECOND, tempo);
+			}
+			return c.getTimeInMillis();
+		}else{
+			return 0;	
+		}
+		
 		
 	}
+	
 }
